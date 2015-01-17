4771fb17d6d4624623a08b4fe1446e546663d97a
zhi.zheng <zhi.zheng@zhenai.com>
2015-01-17 14:54:07 +0800

    文件提交

--- src/java/com/eric/jimiopen/actions/AdvisorAct.java
+++ src/java/com/eric/jimiopen/actions/AdvisorAct.java
@@ -1,6 +1,13 @@
 package com.eric.jimiopen.actions;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 
+@Controller
+@RequestMapping("/advisor")
 public class AdvisorAct {
 	
+	private static final Logger log = LoggerFactory.getLogger(AdvisorAct.class);
 }

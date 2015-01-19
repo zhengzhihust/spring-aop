bb1916dcb3ee1d544fb9275419d9a6e488245882
zhi.zheng <zhi.zheng@zhenai.com>
2015-01-19 09:28:28 +0800

    合并代码

---
+++ src/java/com/eric/jimiopen/modules/register/vo/RegVoTransformator.java
@@ -0,0 +1,27 @@
+package com.eric.jimiopen.modules.register.vo;
+
+import java.io.Serializable;
+
+import com.eric.jimiopen.modules.register.bean.MemberBaseInfo;
+import com.eric.jimiopen.modules.register.bean.MemberLoginInfo;
+import com.eric.jimiopen.modules.register.bean.MemberPhone;
+
+public class RegVoTransformator implements Serializable{
+	
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 7809424673293566724L;
+
+	public static MemberBaseInfo assemblyBaseInfo(MemberResigterVo resigterVo){
+		return null;
+	}
+	
+	public static MemberLoginInfo assemblyLoginInfo(MemberResigterVo resigterVo){
+		return null;
+	}
+	
+	public static MemberPhone assemblyPhoneInfo(MemberResigterVo resigterVo){
+		return null;
+	}
+}
